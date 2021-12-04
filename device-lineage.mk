7f43d2e1b66fb54f7a5e7232e3702efdcd8b24d7
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-05 05:07:40 +0530

    Add device manifest for radio
    Change-Id: Ib6c4b28d33fb64cb05c715d86a96a829a498beac

--- device-lineage.mk
+++ device-lineage.mk
@@ -7,3 +7,7 @@
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# Radio
+DEVICE_MANIFEST_FILE += \
+    device/google/raviole/manifest_radio.xml

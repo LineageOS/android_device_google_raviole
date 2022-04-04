3886c11375d63240b4bd92d1d435020f08191a28
Chirayu Desai <chirayudesai1@gmail.com>
2022-04-05 03:32:42 +0530

    Fix regen-vendor.sh
    Change-Id: I33206a028a863d9849d49a6b0ed44661e03d6e0c

--- oriole/regen-vendor.sh
+++ oriole/regen-vendor.sh
@@ -1131,6 +1131,9 @@ VENDOR_SKIP_FILES=(
     "lib64/libmemunreachable.so"
 )
 
+# Initialize the helper
+setup_vendor_deps "${ANDROID_ROOT}"
+
 generate_prop_list_from_image "${_input_image}" "${_output_file}" VENDOR_SKIP_FILES
 
 # Fixups

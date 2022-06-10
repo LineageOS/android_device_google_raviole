2db0ea401c1b2b44381177e7bbeff1f9b6fd1efe
Michael Bestas <mkbestas@lineageos.org>
2022-06-10 05:14:06 +0300

    raviole: Inline kernel building
    Change-Id: I629c70b74db44d02847212b774982060ab84f1d2

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,4 +6,17 @@
 
 BUILD_BROKEN_DUP_RULES := true
 
+# Kernel
+TARGET_KERNEL_DTBO_TARGET_PREFIX := dts/
+TARGET_KERNEL_DTBO_TARGET := google/dtbo.img
+TARGET_KERNEL_DTB_TARGET := \
+    google/gs101-a0.dtb \
+    google/gs101-b0.dtb
+TARGET_KERNEL_SOURCE := kernel/google/raviole
+
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat device/google/raviole/vendor_dlkm.modules.load))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat device/google/raviole/vendor_boot.modules.load))
+
+# Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml

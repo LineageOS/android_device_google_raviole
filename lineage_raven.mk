eecbb6cc104512c0cfbc48e7b41991b66ab59ad8
Michael Bestas <mkbestas@lineageos.org>
2022-06-10 05:12:13 +0300

    Update fingerprint/build description from SQ3A.220605.009.B1
    Change-Id: Iaf930a8f9c64a7a5a449fa6f4fc0a7aa9c6a1c5b

--- lineage_raven.mk
+++ lineage_raven.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
-    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220305.013.A3 8229987 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220605.009.B1 8650216 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220305.013.A3/8229987:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)

43a9921cd913d21a74fb4e84aebf973f1941b29c
Neel Chauhan <neel@neelc.org>
2022-07-21 08:38:40 -0700

    raviole: Don't use prebuilt kernel, build our own
    AOSP expects an externally built kernel for raviole, but we want to
    build inline, so do this instead.
    
    Change-Id: Ia1ee2e7a84cba6bcbc77fb40aba20dcd8debe299

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,4 +6,14 @@
 
 BUILD_BROKEN_DUP_RULES := true
 
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1
+TARGET_KERNEL_CONFIG := raviole_defconfig
+TARGET_NEEDS_DTBOIMAGE := true
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_ARCH := arm64
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/google/raviole
+
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml

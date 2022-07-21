8ed5f6abb9d29d86e58d496e708334092cf8e330
Neel Chauhan <neel@neelc.org>
2022-07-21 08:38:40 -0700

    raviole: Build external modules
    Change-Id: Ia1ee2e7a84cba6bcbc77fb40aba20dcd8debe299

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -21,5 +21,29 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/googl
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(foreach m,$(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
 BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
 
+TARGET_KERNEL_EXT_MODULE_ROOT := kernel/google/raviole/private/google-modules
+TARGET_KERNEL_EXT_MODULES := \
+    amplifiers/audiometrics \
+    amplifiers/cs35l41 \
+    amplifiers/cs40l25 \
+    amplifiers/drv2624 \
+    aoc \
+    aoc/alsa \
+    aoc/usb \
+    bluetooth/broadcom \
+    bms \
+    display/samsung \
+    edgetpu/drivers/edgetpu \
+    gpu/mali_kbase \
+    gpu/mali_pixel \
+    lwis \
+    nfc \
+    power/reset \
+    touch/common \
+    touch/fts/ftm5 \
+    touch/sec \
+    uwb/kernel \
+    wlan/bcmdhd4389
+
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml

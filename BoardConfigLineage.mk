a124dbb0bea43f900be3ff97831683ca359a0c66
Neel Chauhan <neel@neelc.org>
2022-07-21 08:38:40 -0700

    raviole: Don't use prebuilt kernel, build our own
    Change-Id: Ia1ee2e7a84cba6bcbc77fb40aba20dcd8debe299

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,4 +6,44 @@
 
 BUILD_BROKEN_DUP_RULES := true
 
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1
+TARGET_KERNEL_CONFIG := raviole_defconfig
+TARGET_NEEDS_DTBOIMAGE := true
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_ARCH := arm64
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/google/raviole
+
+# GKI
+TARGET_EXTERNAL_MODULE_PATH := private/google-modules
+TARGET_EXTERNAL_MODULES := \
+	nfc \
+	nfc/ese \
+	display/samsung \
+	uwb/kernel \
+	gpu/mali_kbase \
+	gpu/mali_pixel \
+	lwis \
+	edgetpu/drivers/edgetpu \
+	touch/common \
+	touch/sec \
+	touch/fts/ftm5 \
+	fingerprint/fpc \
+	aoc \
+	aoc/alsa \
+	aoc/usb \
+	wlan/bcmdhd4389 \
+	wlan/bcmdhd43752 \
+	bluetooth/broadcom \
+	bms \
+	power/reset \
+	amplifiers/cs35l45 \
+	amplifiers/cs40l26 \
+	amplifiers/drv2624 \
+	amplifiers/audiometrics \
+	amplifiers/cs40l25 \
+	amplifiers/cs35l41
+
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml

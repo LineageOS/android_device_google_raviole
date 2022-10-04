e4580dfe4fd43ddc4468455ce71aa033b9ef2701
Michael Bestas <mkbestas@lineageos.org>
2022-10-04 04:41:01 +0300

    [SQUASH] Merge tag 'android-13.0.0_r7' into staging/lineage-20_merge-android-13.0.0_r7
    Change-Id: Ic2d96e0d89b5e53d6f48d7f67938494c816ff167

--- device-raven.mk
+++ device-raven.mk
@@ -175,7 +175,7 @@ endif
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=37
+    ro.vendor.build.svn=39
 
 # Set support hide display cutout feature
 PRODUCT_PRODUCT_PROPERTIES += \

eacf25361654f86d636dbaf0fe98c4f20dd10951
Michael Bestas <mkbestas@lineageos.org>
2022-12-09 02:35:17 +0200

    Update fingerprint/build description from TQ1A.221205.011
    Change-Id: Ia84efe7e830529710e48fdbdaf1d0b5a6ca69bc3

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=oriole \
-    PRIVATE_BUILD_DESC="oriole-user 13 TP1A.221105.002 9080065 release-keys"
+    PRIVATE_BUILD_DESC="oriole-user 13 TQ1A.221205.011 9244662 release-keys"
 
-BUILD_FINGERPRINT := google/oriole/oriole:13/TP1A.221105.002/9080065:user/release-keys
+BUILD_FINGERPRINT := google/oriole/oriole:13/TQ1A.221205.011/9244662:user/release-keys
 
 $(call inherit-product, vendor/google/oriole/oriole-vendor.mk)

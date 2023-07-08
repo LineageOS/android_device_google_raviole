1dd608d7ce17086f97722497f081adda3778cbd1
Michael Bestas <mkbestas@lineageos.org>
2023-07-08 13:17:34 +0300

    Update fingerprint/build description from TQ3A.230705.001
    Change-Id: I19372ebe4ed688f6a6b8fc2716df8794c2f15c9d

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=oriole \
-    PRIVATE_BUILD_DESC="oriole-user 13 TQ3A.230605.010 10121037 release-keys"
+    PRIVATE_BUILD_DESC="oriole-user 13 TQ3A.230705.001 10216780 release-keys"
 
-BUILD_FINGERPRINT := google/oriole/oriole:13/TQ3A.230605.010/10121037:user/release-keys
+BUILD_FINGERPRINT := google/oriole/oriole:13/TQ3A.230705.001/10216780:user/release-keys
 
 $(call inherit-product, vendor/google/oriole/oriole-vendor.mk)
--- lineage_raven.mk
+++ lineage_raven.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
-    PRIVATE_BUILD_DESC="raven-user 13 TQ3A.230605.010 10121037 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 13 TQ3A.230705.001 10216780 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:13/TQ3A.230605.010/10121037:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:13/TQ3A.230705.001/10216780:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)

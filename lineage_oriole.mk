0d08f89a55dea05daaff595805ac803a70b389d1
Michael Bestas <mkbestas@lineageos.org>
2024-04-03 21:31:11 +0300

    Update fingerprint/build description from AP1A.240405.002
    Change-Id: Ied6a156869e3c93ea4204e1e67f2e9fe11055bcc

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=oriole \
-    PRIVATE_BUILD_DESC="oriole-user 14 AP1A.240305.019.A1 11445699 release-keys"
+    PRIVATE_BUILD_DESC="oriole-user 14 AP1A.240405.002 11480754 release-keys"
 
-BUILD_FINGERPRINT := google/oriole/oriole:14/AP1A.240305.019.A1/11445699:user/release-keys
+BUILD_FINGERPRINT := google/oriole/oriole:14/AP1A.240405.002/11480754:user/release-keys
 
 $(call inherit-product, vendor/google/oriole/oriole-vendor.mk)

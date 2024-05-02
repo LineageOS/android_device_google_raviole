4d90bc1647d2c99362abad405a25df4c8c9526a2
Nelson Li <nelsonli@google.com>
2024-05-02 07:30:50 +0000

    Refactor product inheritance for slider products
    Add an additional layer of inheritance for `aosp_slider`,
    `full_slider`, and `aosp_slider_hwasan`.
    
    This change adds a common layer of inheritance for non-factory
    products (slider_generic.mk). This makes it easier to configure
    factory and non-factory products differently.
    
    Non-factory `slider` products now inherits from
    `device/google/raviole/slider_generic.mk` which inherit from
    `device/google/raviole/device-slider.mk`.
    
    aconfig flags: Flag: EXEMPT mechanical refactoring
    build flags: Flag: EXEMPT mechanical refactoring
    
    Bug: 338332183
    Bug: 338333277
    Test: lunch aosp_slider-trunk_staging-userdebug && m
          lunch full_slider-trunk_staging-userdebug && m
          lunch aosp_slider_hwasan-trunk_staging-userdebug && m
    Change-Id: If0bbcdf2d4de3c2eca14e514b863f7416ef12e45

--- slider_generic.mk
+++ slider_generic.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2019 The Android Open-Source Project
+# Copyright 2024 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,14 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/google/gs101/aosp_common.mk)
-$(call inherit-product, device/google/raviole/slider_generic.mk)
 
-PRODUCT_NAME := aosp_slider
-PRODUCT_DEVICE := slider
-PRODUCT_MODEL := AOSP on Slider
-PRODUCT_BRAND := Android
-PRODUCT_MANUFACTURER := Google
-
-# Most sliders don't have dauntless chip, set it as default
-BOARD_WITHOUT_DTLS := true
+$(call inherit-product, device/google/raviole/device-slider.mk)

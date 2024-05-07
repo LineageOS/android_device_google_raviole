0f5b6c67a6bab4a34e1adcf7708247c551cd29bf
Michael Bestas <mkbestas@lineageos.org>
2024-05-08 02:27:07 +0300

    Update fingerprint/build description from AP1A.240505.004
    Change-Id: Id1ac851d9d2dccb40bc3fcc141861131608edb91

--- lineage_raven.mk
+++ lineage_raven.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
-    PRIVATE_BUILD_DESC="raven-user 14 AP1A.240405.002 11480754 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 14 AP1A.240505.004 11583682 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:14/AP1A.240405.002/11480754:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:14/AP1A.240505.004/11583682:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)

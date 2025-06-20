f9570f532ad4558e5add611280157a4ce1c9e202
Michael Bestas <mkbestas@gmail.com>
2025-06-20 18:57:50 +0300

    raviole: Move wfc-pkt-router to gs-common
    Change-Id: I73ed5e5565fd5b786a6931d6755c5db74842877b

--- device-raven.mk
+++ device-raven.mk
@@ -33,7 +33,6 @@ DEVICE_PACKAGE_OVERLAYS += device/google/raviole/raven/overlay
 
 include device/google/raviole/audio/raven/audio-tables.mk
 include device/google/gs101/device-shipping-common.mk
-include device/google/gs101/telephony/pktrouter.mk
 include device/google/gs-common/bcmbt/bluetooth.mk
 include device/google/gs-common/touch/lsi/lsi.mk
 

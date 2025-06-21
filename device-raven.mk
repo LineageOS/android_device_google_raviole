bb65efe2f3cd05e43cb66c3acca4556c01db01e0
Michael Bestas <mkbestas@gmail.com>
2025-06-21 10:45:41 +0300

    raviole: Switch to our own kernel
    Change-Id: Iaec4d3704b2b44cd809f24d922f6965312da71e5

--- device-raven.mk
+++ device-raven.mk
@@ -23,9 +23,9 @@ $(call soong_config_set,raviole_bootloader,prebuilt_dir,$(RELEASE_GOOGLE_BOOTLOA
 
 
 TARGET_LINUX_KERNEL_VERSION := $(RELEASE_KERNEL_RAVEN_VERSION)
-# Keeps flexibility for kasan and ufs builds
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_RAVEN_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_RAVEN_DIR)/kernel-headers
+TARGET_KERNEL_DEVICE := raviole
+TARGET_KERNEL_DIR := device/google/$(TARGET_KERNEL_DEVICE)-kernels/$(TARGET_LINUX_KERNEL_VERSION)
+TARGET_KERNEL_PLATFORM_SOURCE := google/gs-$(TARGET_LINUX_KERNEL_VERSION)
 
 $(call inherit-product, device/google/raviole/uwb/uwb_calibration_country.mk)
 
@@ -54,14 +54,6 @@ PRODUCT_COPY_FILES += \
 # Recovery files
 PRODUCT_COPY_FILES += \
 	device/google/gs101/conf/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.raven.rc
-
-# insmod files. Kernel 5.10 prebuilts don't provide these yet, so provide our
-# own copy if they're not in the prebuilts.
-# TODO(b/369686096): drop this when 5.10 is gone.
-ifeq ($(wildcard $(TARGET_KERNEL_DIR)/init.insmod.*.cfg),)
-PRODUCT_COPY_FILES += \
-	device/google/raviole/init.insmod.raven.cfg:$(TARGET_COPY_OUT_VENDOR_DLKM)/etc/init.insmod.raven.cfg
-endif
 
 # Thermal Config
 PRODUCT_COPY_FILES += \

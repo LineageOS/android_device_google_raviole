708cf6af22b7fff8d8f3589e6fcc4daf007a36c0
Michael Bestas <mkbestas@lineageos.org>
2025-11-22 13:47:42 +0200

    Update fingerprint/build description from BP3A.250905.014
    Change-Id: Ie4ebf9610d19d7cbcad66667d60b1b7c4f9e1ae6

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -26,8 +26,8 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="oriole-user 16 BP2A.250605.031.A5 13701493 release-keys" \
-    BuildFingerprint=google/oriole/oriole:16/BP2A.250605.031.A5/13701493:user/release-keys \
+    BuildDesc="oriole-user 16 BP3A.250905.014 13873947 release-keys" \
+    BuildFingerprint=google/oriole/oriole:16/BP3A.250905.014/13873947:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
--- lineage_raven.mk
+++ lineage_raven.mk
@@ -26,8 +26,8 @@ TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="raven-user 16 BP2A.250605.031.A5 13701493 release-keys" \
-    BuildFingerprint=google/raven/raven:16/BP2A.250605.031.A5/13701493:user/release-keys \
+    BuildDesc="raven-user 16 BP3A.250905.014 13873947 release-keys" \
+    BuildFingerprint=google/raven/raven:16/BP3A.250905.014/13873947:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)

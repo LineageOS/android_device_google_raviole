946deb11c4f288f1284a3f5bbebbf301ec5a8cf9
Michael Bestas <mkbestas@lineageos.org>
2025-11-25 14:30:41 +0200

    raviole: Cleanup PRODUCT_PACKAGES
    Tag is already included in build/make.
    Iwlan is already included in device/google/gs101.
    Don't explicitly build vendor libs.
    
    Change-Id: I60f1d459acc1c630e61fb3d2f32e63a931456fb6

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,10 +14,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
-# IWLAN
-PRODUCT_PACKAGES += \
-    Iwlan
-
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 

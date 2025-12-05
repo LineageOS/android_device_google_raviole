1485dad56b4bc6be4e80ee06cbf5fb00cc3264ba
Michael Bestas <mkbestas@lineageos.org>
2025-12-05 09:17:34 +0200

    raviole: Commonize BoardConfig configuration
    Change-Id: I69975c928a0d5bb74dfc9e0e5685d5319d1048b0

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,27 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include device/google/gs101/BoardConfig-common.mk
+
+# Kernel
+BOARD_KERNEL_CMDLINE += disable_dma32=on
+
+# Kernel modules
+BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
+
+BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
+BOARD_KERNEL_CMDLINE += exynos_mfc.load_sequential=1
+BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
+BOARD_KERNEL_CMDLINE += pcie-exynos-core.load_sequential=1
+BOARD_KERNEL_CMDLINE += g2d.load_sequential=1
+
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/recovery/modules.blocklist.vendor_boot
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/recovery/modules.load.vendor_boot))
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
+
+# WiFi
+include device/google/gs101/wifi/BoardConfig-wifi.mk
--- raven/BoardConfig.mk
+++ raven/BoardConfig.mk
@@ -5,30 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Enable load module in parallel
-BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
-
-# The modules which need to be loaded in sequential
-BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
-BOARD_KERNEL_CMDLINE += exynos_mfc.load_sequential=1
-BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
-BOARD_KERNEL_CMDLINE += pcie-exynos-core.load_sequential=1
-BOARD_KERNEL_CMDLINE += g2d.load_sequential=1
-
-TARGET_BOARD_INFO_FILE := device/google/raviole/board-info.txt
-TARGET_BOOTLOADER_BOARD_NAME := raven
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := $(DEVICE_CODENAME)
 TARGET_SCREEN_DENSITY := 560
 
-BOARD_KERNEL_CMDLINE += disable_dma32=on
-
-include device/google/gs101/BoardConfig-common.mk
+# SEPolicy
 include device/google/raviole/sepolicy/raven-sepolicy.mk
-include device/google/gs101/wifi/BoardConfig-wifi.mk
 
-# Kernel modules
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/recovery/modules.blocklist.vendor_boot
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/recovery/modules.load.vendor_boot))
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
+include $(DEVICE_PATH)/BoardConfigCommon.mk
 
 include $(VENDOR_PATH)/BoardConfigVendor.mk

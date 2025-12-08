2be34b37c81948d589aaf28212b888457e409a3c
Michael Bestas <mkbestas@lineageos.org>
2025-12-08 22:40:30 +0200

    raviole: Remove unnecessary soong namespace imports
    Change-Id: I7313152a16e7a3f35b8ec8117af305d606f21f3e

--- oriole/extract-files.py
+++ oriole/extract-files.py
@@ -27,7 +27,6 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/google/raviole',
     'hardware/google/av',
     'hardware/google/interfaces',
     'hardware/google/pixel',

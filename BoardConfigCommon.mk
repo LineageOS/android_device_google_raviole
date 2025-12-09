a2e552d3399f6992e8b0279b2dc4367f5bd3a647
Michael Bestas <mkbestas@lineageos.org>
2025-12-10 01:15:30 +0200

    SPL: BP4A.251205.006
    Change-Id: I218e66b69b8500a6ff8d3b2c0521715ae1100ab7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,7 +5,7 @@
 #
 
 # Security - must be defined before including BoardConfig-common.mk
-BOOT_SECURITY_PATCH := 2025-09-05
+BOOT_SECURITY_PATCH := 2025-12-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 include device/google/gs101/BoardConfig-common.mk

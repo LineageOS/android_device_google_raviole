2a8f377b0198ded35f1476f183d793674350a21a
Michael Bestas <mkbestas@lineageos.org>
2026-02-23 00:04:49 +0200

    raviole: Inherit from gs101 common.mk
    Change-Id: I66992c62c7d299fd9c4071dc9a27463c4cebd4c3

--- device-raven.mk
+++ device-raven.mk
@@ -12,7 +12,7 @@ TARGET_KERNEL_DIR := device/google/$(TARGET_KERNEL_DEVICE)-kernels/$(TARGET_LINU
 TARGET_KERNEL_PLATFORM_SOURCE := google/gs-$(TARGET_LINUX_KERNEL_VERSION)
 
 # Inherit from gs101
-include device/google/gs101/device-shipping-common.mk
+include device/google/gs101/common.mk
 
 # Overlays
 PRODUCT_PACKAGES += \
